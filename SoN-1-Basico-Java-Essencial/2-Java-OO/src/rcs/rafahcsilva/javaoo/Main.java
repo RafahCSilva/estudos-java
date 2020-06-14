8105141cafd144b4511f25397acca24ee3e4046b
RafahCSilva <rafahcsilva@gmail.com>
2020-06-14 00:45:37 -0300

    methods

--- SoN-1-Basico-Java-Essencial/2-Java-OO/src/rcs/rafahcsilva/javaoo/Main.java
+++ SoN-1-Basico-Java-Essencial/2-Java-OO/src/rcs/rafahcsilva/javaoo/Main.java
@@ -3,11 +3,15 @@ package rcs.rafahcsilva.javaoo;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
         Person person = new Person();
+
         person.name = "Rafao";
         person.age = 26;
         person.cpf = "123.123.123-12";
+
+        person.say();
+
+        System.out.println(Person.walk());
 
         System.out.println("Name: " + person.name);
     }
