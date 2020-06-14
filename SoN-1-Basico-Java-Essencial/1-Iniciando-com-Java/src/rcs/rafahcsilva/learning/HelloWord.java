116e6c7bc9ebc97a44c30aa418f07e81354861ec
RafahCSilva <rafahcsilva@gmail.com>
2020-06-13 23:37:37 -0300

    variaveis e tipos

--- SoN-1-Basico-Java-Essencial/1-Iniciando-com-Java/src/rcs/rafahcsilva/learning/HelloWord.java
+++ SoN-1-Basico-Java-Essencial/1-Iniciando-com-Java/src/rcs/rafahcsilva/learning/HelloWord.java
@@ -1,7 +1,27 @@
 package rcs.rafahcsilva.learning;
 
 public class HelloWord {
+    // Atribute
+    public static String myAttr = "Attr";
+
     public static void main(String[] args) {
         System.out.println("Hello world do Rafao");
+
+        // Primitivo
+        int myInt = 0;
+        char myChar = 'C';
+        byte myByte;
+        float myFloat = 0;
+        double myDouble = 0.0;
+
+        // Wrapper
+        Integer myIntW = null;
+        Character myCharW = 'C';
+        Byte myByteW = null;
+        Float myFloatW = null;
+        Double myDoubleW = null;
+        String mySrtW = "Str";
+
+        myAttr = "";
     }
 }
