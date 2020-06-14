b5c322c586ae253d71aecd2b5366092ad5ad8fef
RafahCSilva <rafahcsilva@gmail.com>
2020-06-13 22:26:24 -0300

    Estrutura inicial da trilha do School of Net

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# estudos-java
-Meus Estudos de Java
+# Meus Estudos de Java
+
+## School of Net
+
+https://www.schoolofnet.com/
+
+
+### SoN-1-Basico-Java-Essencial
+
+https://www.schoolofnet.com/plano-de-estudo-java-essencial/
+
+1. Iniciando com Java.
+2. Orientação a Objetos com Java
+3. Estrutura de dados com Java
+4. Api's Nativas no Java
+5. Java com JDBC.
+6. Java Swing
+7. Iniciando com Java FX
+8. Java 8
+
+
+### SoN-2-Avancado-Java-e-BD
+
+https://www.schoolofnet.com/plano-de-estudo-java-e-banco-de-dados/
+
+1. Java com JDBC.
+2. Java com MongoDB.
+3. Iniciando Hibernate
+
+
+### SoN-3-Avancado-Spring-Boot
+
+https://www.schoolofnet.com/plano-de-estudo-spring-boot-developer/
+
+1. Iniciando com SpringBoot Rev3
+2. Spring Boot Intermediário
+3. Iniciando com SpringBoot Thymeleaf
+4. Criando um Helpdesk com SpringBoot e MySQL
+
+
---
+++ create_structure.sh
@@ -0,0 +1,23 @@
+#!/bin/ash
+
+mkdir -p SoN-1-Basico-Java-Essencial
+mkdir -p SoN-1-Basico-Java-Essencial/1-Iniciando-com-Java
+mkdir -p SoN-1-Basico-Java-Essencial/2-Java-POO
+mkdir -p SoN-1-Basico-Java-Essencial/3-Estrutura-de-Dados-com-Java
+mkdir -p SoN-1-Basico-Java-Essencial/4-Apis-Nativas-no-Java
+mkdir -p SoN-1-Basico-Java-Essencial/5-Java-JDBC
+mkdir -p SoN-1-Basico-Java-Essencial/6-Java-Swing
+mkdir -p SoN-1-Basico-Java-Essencial/7-Java-FX
+mkdir -p SoN-1-Basico-Java-Essencial/8-Java-8
+
+mkdir -p SoN-2-Avancado-Java-e-BD
+mkdir -p SoN-2-Avancado-Java-e-BD/1-Java-com-JDBC
+mkdir -p SoN-2-Avancado-Java-e-BD/2-Java-com-MongoDB
+mkdir -p SoN-2-Avancado-Java-e-BD/3-Iniciando-Hibernate
+
+mkdir -p SoN-3-Avancado-Spring-Boot
+mkdir -p SoN-3-Avancado-Spring-Boot/1-Iniciando-com-SpringBoot-Rev3
+mkdir -p SoN-3-Avancado-Spring-Boot/2-Spring-Boot-Intermediário
+mkdir -p SoN-3-Avancado-Spring-Boot/3-Iniciando-com-SpringBoot-Thymeleaf
+mkdir -p SoN-3-Avancado-Spring-Boot/4-Criando-um-Helpdesk-com-SpringBoot-e-MySQL
+
