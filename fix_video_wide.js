ac58ac883e9ddc6e7c93c43d92f62794af404338
RafahCSilva <rafahcsilva@gmail.com>
2020-06-13 23:00:35 -0300

    Hack para deixar o video do SoN em wide

---
+++ fix_video_wide.js
@@ -0,0 +1,5 @@
+document.querySelector("#video").setAttribute('style',' position: fixed; top: 0; left: 0; height: 100%; width: 100%; z-index: 10000000;');
+document.querySelector("#video div.media-control div.playback_rate li:nth-child(5) > a").click();
+document.querySelector("#launcher").remove();
+
+javascript:document.querySelector("#video").setAttribute('style',' position: fixed; top: 0; left: 0; height: 100%; width: 100%; z-index: 10000000;');document.querySelector("#video div.media-control div.playback_rate li:nth-child(5) > a").click();document.querySelector("#launcher").remove();
