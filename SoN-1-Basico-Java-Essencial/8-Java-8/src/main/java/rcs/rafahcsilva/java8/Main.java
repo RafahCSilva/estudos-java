7d32f0e997445f0d8ba8603092620bc23c39fb21
RafahCSilva <rafahcsilva@gmail.com>
2020-06-15 01:04:48 -0300

    Lambdas

--- SoN-1-Basico-Java-Essencial/8-Java-8/src/main/java/rcs/rafahcsilva/java8/Main.java
+++ SoN-1-Basico-Java-Essencial/8-Java-8/src/main/java/rcs/rafahcsilva/java8/Main.java
@@ -1,7 +1,41 @@
 package rcs.rafahcsilva.java8;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("oi");
+        System.out.println("Hi Java 8");
+
+        // Runnable
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(" > Hi from thread");
+            }
+        };
+        System.out.println("Starting Thread1...");
+        new Thread(run).start();
+
+        // Lambda
+        Runnable run1 = () -> System.out.println(" > Hi from lambda");
+        System.out.println("Starting Thread2...");
+        new Thread(run1).start();
+        new Thread(() -> System.out.println(" > Hi from Anonymous Lambda")).start();
+
+        // Lambdas in
+        List<String> strs = Arrays.asList("Rafael", "Cardoso", "Silva");
+        for (String s : strs) {
+            System.out.println(s);
+        }
+        strs.forEach(s -> System.out.println(s));
+
+        List<String> result = strs
+                .stream()
+                .filter(s -> s.startsWith("C"))
+                .collect(Collectors.toList());
+        result.forEach(System.out::println);
     }
 }
