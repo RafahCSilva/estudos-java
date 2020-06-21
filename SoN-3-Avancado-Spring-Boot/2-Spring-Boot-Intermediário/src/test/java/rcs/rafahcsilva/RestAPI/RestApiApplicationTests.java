1d90badee4a3d26641987d7838351a767acb2137
RafahCSilva <rafahcsilva@gmail.com>
2020-06-20 23:20:54 -0300

    iniciando o projeto SoN-3-2-Spring-Boot-Intermediário

---
+++ SoN-3-Avancado-Spring-Boot/2-Spring-Boot-Intermediário/src/test/java/rcs/rafahcsilva/RestAPI/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rcs.rafahcsilva.RestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
