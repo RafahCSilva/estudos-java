ede796e0e8f21ccc5c073bfc2d5d5cfc078d5e04
RafahCSilva <rafahcsilva@gmail.com>
2020-06-21 19:45:46 -0300

    Mudanças no Stream

--- SoN-1-Java-Essencial/9-java-8-para-13/src/rcs/rafahcsilva/App.java
+++ SoN-1-Java-Essencial/9-java-8-para-13/src/rcs/rafahcsilva/App.java
@@ -1,7 +1,26 @@
 package rcs.rafahcsilva;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Ola");
+
+        //  Helper do Set
+        //  Antigamente
+        // Set<String> listSet = new HashSet<String>();
+        // listSet.add("ASDF");
+        // listSet.add("ASDF");
+        // listSet.add("ASDF");
+        //  Agora
+        Set<String> listSet = Set.of("A", "B", "C");
+
+
+        // Iterate com seed
+        Stream.iterate(2, number -> number * 2)
+                .limit(40)
+                .forEach(n -> System.out.println(n));
     }
 }
